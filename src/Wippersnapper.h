4dae9b77f3004298695816febb6b4e49f5225e68
brentru <brent@adafruit.com>
2021-10-08 15:38:48 -0400

    ...clang

--- src/Wippersnapper.h
+++ src/Wippersnapper.h
@@ -56,8 +56,8 @@
 
 // Uncomment the following use the staging IO server //
 //#define USE_STAGING                     ///< Use Adafruit IO staging
-//certificate #define IO_MQTT_SERVER "io.adafruit.us" ///< Adafruit IO MQTT
-//Server
+// certificate #define IO_MQTT_SERVER "io.adafruit.us" ///< Adafruit IO MQTT
+// Server
 
 #define IO_MQTT_SERVER "io.adafruit.com" ///< Adafruit IO MQTT Server
 //(Production)
