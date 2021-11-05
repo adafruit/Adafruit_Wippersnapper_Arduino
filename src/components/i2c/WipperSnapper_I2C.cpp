c30d605f7c9c145ea0ad9c4eb7f4435b2b33cdc7
brentru <brent@adafruit.com>
2021-11-05 11:02:49 -0400

    strip update() out, not part of init

--- src/components/i2c/WipperSnapper_I2C.cpp
+++ src/components/i2c/WipperSnapper_I2C.cpp
@@ -233,45 +233,5 @@ bool WipperSnapper_Component_I2C::deinitI2CDevice(
 */
 /*******************************************************************************/
 void WipperSnapper_Component_I2C::update() {
-  for (int i = 0; i < drivers.size(); i++) {
-    // Empty wippersnapper_i2c_v1_I2CSensorEvent container message
-    wippersnapper_i2c_v1_I2CSensorEvent sensorEvent =
-        wippersnapper_i2c_v1_I2CSensorEvent_init_zero;
-
-    // Check if i2c device has a temperature sensor
-    if (drivers[i]->getTempSensorPeriod() > -1L) {
-      long curTime = millis(); // take the current time
-      if (curTime - drivers[i]->getTempSensorPeriodPrv() >
-          drivers[i]->getTempSensorPeriod()) {
-        // Update temperature sensor and fill field
-        wippersnapper_i2c_v1_SensorEvent sensorEventMsg =
-            wippersnapper_i2c_v1_SensorEvent_init_zero;
-        drivers[i]->updateTempSensor(&sensorEventMsg.event_data.temperature);
-        WS_DEBUG_PRINT("Read Temperature Sensor Value: ");
-        WS_DEBUG_PRINT(sensorEventMsg.event_data.temperature);
-        WS_DEBUG_PRINTLN(" Degrees C");
-        // TODO: Pack into the sensorEvent message
-      }
-    }
-
-    // Check if i2c device has a humidity sensor
-    if (drivers[i]->getHumidSensorPeriod() > -1L) {
-      long curTime = millis(); // take the current time
-      if (curTime - drivers[i]->getHumidSensorPeriodPrv() >
-          drivers[i]->getHumidSensorPeriod()) {
-        // Update temperature sensor and fill field
-        wippersnapper_i2c_v1_SensorEvent sensorEventMsg =
-            wippersnapper_i2c_v1_SensorEvent_init_zero;
-        drivers[i]->updateHumidSensor(
-            &sensorEventMsg.event_data.relative_humidity);
-        WS_DEBUG_PRINT("Read Humidity Sensor Value: ");
-        WS_DEBUG_PRINT(sensorEventMsg.event_data.temperature);
-        WS_DEBUG_PRINTLN(" %RH");
-      }
-    }
-
-    // TODO //
-    // Fill the container I2CSensorEvent -> Signal proto
-    // Publish proto
-  }
+  // TODO
 }
