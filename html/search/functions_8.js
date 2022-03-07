49495667004d05c087c9592c517e12b31f5ad061
Doxygen CI <ci-arduino@invalid>
2022-03-07 22:25:53 +0000

    Deploy docs to GitHub Pages from commit 6b579d4a71
    Commit: 6b579d4a71370fe32afc83d318745b6f60b2c50c
    GitHub Actions run: 1948244596

--- html/search/functions_8.js
+++ html/search/functions_8.js
@@ -5,5 +5,5 @@ var searchData=
   ['initdigitalpin',['initDigitalPin',['../class_wippersnapper___digital_g_p_i_o.html#ac0f84fa90b9a79ccf94a7b3ba6271de4',1,'Wippersnapper_DigitalGPIO']]],
   ['initi2cdevice',['initI2CDevice',['../class_wipper_snapper___component___i2_c.html#abf276df7870551bab8283381c6ca55df',1,'WipperSnapper_Component_I2C']]],
   ['initializei2cbus',['initializeI2CBus',['../_wippersnapper_8cpp.html#a0bf599ce0a2501b5e12be07891208947',1,'Wippersnapper.cpp']]],
-  ['isinitialized',['isInitialized',['../class_wipper_snapper___component___i2_c.html#a437cd9d59c7085b4cf90a6b90e167903',1,'WipperSnapper_Component_I2C::isInitialized()'],['../class_wipper_snapper___i2_c___driver.html#a5221855537a1daeebd31ff5d530dd8a4',1,'WipperSnapper_I2C_Driver::isInitialized()']]]
+  ['isinitialized',['isInitialized',['../class_wipper_snapper___i2_c___driver.html#a5221855537a1daeebd31ff5d530dd8a4',1,'WipperSnapper_I2C_Driver::isInitialized()'],['../class_wipper_snapper___component___i2_c.html#a437cd9d59c7085b4cf90a6b90e167903',1,'WipperSnapper_Component_I2C::isInitialized()']]]
 ];
