81d48f8c356f19837d60573c5b7e4e7329edf582
brentru <brent@adafruit.com>
2022-03-08 15:52:14 -0500

    bump lib version, ptr

--- src/components/i2c/drivers/WipperSnapper_I2C_Driver_MCP9808.h
+++ src/components/i2c/drivers/WipperSnapper_I2C_Driver_MCP9808.h
@@ -16,7 +16,7 @@
 #define WipperSnapper_I2C_Driver_MCP9808_H
 
 #include "WipperSnapper_I2C_Driver.h"
-#include <Adafruit_MCP9808>
+#include <Adafruit_MCP9808.h>
 
 /**************************************************************************/
 /*!
@@ -25,7 +25,6 @@
 /**************************************************************************/
 class WipperSnapper_I2C_Driver_MCP9808 : public WipperSnapper_I2C_Driver {
 public:
-
   /*******************************************************************************/
   /*!
       @brief    Constructor for a MCP9808 sensor.
@@ -40,7 +39,7 @@ public:
     // Called when a MCP9808 component is created
     setI2CAddress(sensorAddress); // sets the driver's I2C address
     _mcp9808 = new Adafruit_MCP9808();
-    _isInitialized = _mcp9808.begin();
+    _isInitialized = _mcp9808->begin();
   }
 
   /*******************************************************************************/
