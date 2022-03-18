dd8a3dc7881c10364b754166bd2786890d0ebac1
brentru <brent@adafruit.com>
2022-03-18 10:07:31 -0400

    bump ver

--- src/Wippersnapper.h
+++ src/Wippersnapper.h
@@ -62,7 +62,7 @@
 #endif
 
 #define WS_VERSION                                                             \
-  "1.0.0-beta.28" ///< WipperSnapper app. version (semver-formatted)
+  "1.0.0-beta.29" ///< WipperSnapper app. version (semver-formatted)
 
 // Reserved Adafruit IO MQTT topics
 #define TOPIC_IO_THROTTLE "/throttle" ///< Adafruit IO Throttle MQTT Topic
