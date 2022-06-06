78e711a993ac5a4e756812f8c9c2ef092dc40373
TheGodMotherG <98500689+TheGodMotherG@users.noreply.github.com>
2022-06-06 01:06:06 -0400

    Update WipperSnapper_I2C.cpp

--- src/components/i2c/WipperSnapper_I2C.cpp
+++ src/components/i2c/WipperSnapper_I2C.cpp
@@ -227,16 +227,16 @@ bool WipperSnapper_Component_I2C::initI2CDevice(
     _bme280->configureDriver(msgDeviceInitReq);
     drivers.push_back(_bme280);
     WS_DEBUG_PRINTLN("BME280 Initialized Successfully!");
-  } else if (strcmp("sht4x", msgDeviceInitReq->i2c_device_name) == 0) {
-    _sht4x = new WipperSnapper_I2C_Driver_SHT4x(this->_i2c, i2cAddress);
-    if (!_sht4x->begin()) {
+  } else if (strcmp("SHT4x", msgDeviceInitReq->i2c_device_name) == 0) {
+    _SHT4x = new WipperSnapper_I2C_Driver_SHT4x(this->_i2c, i2cAddress);
+    if (!_SHT4x->begin()) {
       WS_DEBUG_PRINTLN("ERROR: Failed to initialize SHT4x!");
       _busStatusResponse =
           wippersnapper_i2c_v1_BusResponse_BUS_RESPONSE_DEVICE_INIT_FAIL;
       return false;
     }
-    _sht4x->configureDriver(msgDeviceInitReq);
-    drivers.push_back(_sht4x);
+    _SHT4x->configureDriver(msgDeviceInitReq);
+    drivers.push_back(_SHT4x);
     WS_DEBUG_PRINTLN("SHT4x Initialized Successfully!");
 
   } else if (strcmp("dps310", msgDeviceInitReq->i2c_device_name) == 0) {
