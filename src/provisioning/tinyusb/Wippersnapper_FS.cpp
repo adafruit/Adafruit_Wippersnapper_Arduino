ab0d70667ad3a07cb8c21e5af9ec5576bed84b89
brentru <brent@adafruit.com>
2022-11-18 13:33:51 -0500

    refactor _fs

--- src/provisioning/tinyusb/Wippersnapper_FS.cpp
+++ src/provisioning/tinyusb/Wippersnapper_FS.cpp
@@ -341,7 +341,7 @@ void Wippersnapper_FS::parseSecrets() {
     WS_DEBUG_PRINTLN("ERROR: invalid io_username value in secrets.json!");
     writeToBootOut("ERROR: invalid io_username value in secrets.json!\n");
     while (1) {
-      statusLEDSolid(WS_LED_STATUS_FS_WRITE);
+      showSolidStatusLED(WS_LED_STATUS_FS_WRITE);
       yield();
     }
   }
@@ -398,7 +398,7 @@ void Wippersnapper_FS::parseSecrets() {
     WS_DEBUG_PRINTLN("ERROR: invalid network_ssid value in secrets.json!");
     writeToBootOut("ERROR: invalid network_ssid value in secrets.json!\n");
     while (1) {
-      statusLEDSolid(WS_LED_STATUS_FS_WRITE);
+      showSolidStatusLED(WS_LED_STATUS_FS_WRITE);
       yield();
     }
   }
@@ -493,7 +493,7 @@ void Wippersnapper_FS::writeToBootOut(PGM_P str) {
 /**************************************************************************/
 void Wippersnapper_FS::fsHalt() {
   while (1) {
-    statusLEDSolid(WS_LED_STATUS_FS_WRITE);
+    showSolidStatusLED(WS_LED_STATUS_FS_WRITE);
     delay(1000);
     yield();
   }
