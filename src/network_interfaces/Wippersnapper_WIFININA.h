78dadfcd24a7c05f423031b739e6f6ee9519b098
brentru <brent@adafruit.com>
2022-11-30 16:38:09 -0500

    fix error: 'class WiFiClass' has no member named 'mode'

--- src/network_interfaces/Wippersnapper_WIFININA.h
+++ src/network_interfaces/Wippersnapper_WIFININA.h
@@ -118,7 +118,6 @@ public:
   bool check_valid_ssid() {
     // Set WiFi to station mode and disconnect from an AP if it was previously
     // connected
-    WiFi.mode(WIFI_STA);
     WiFi.disconnect();
     delay(100);
 
