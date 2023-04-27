991efc9d0df963255c38d3206024bb68a61a18c8
brentru <brent@adafruit.com>
2023-04-27 13:54:47 -0400

    add fonts/symbols and tooltips

---
+++ src/display/ws_display_tooltips.h
@@ -0,0 +1,23 @@
+/*!
+ * @file ws_loading_tooltips.h
+ *
+ * Wippersnapper tooltips for the loading screen on a display
+ *
+ * Adafruit invests time and resources providing this open source code,
+ * please support Adafruit and open-source hardware by purchasing
+ * products from Adafruit!
+ *
+ * Copyright (c) Brent Rubell 2023 for Adafruit Industries.
+ *
+ * BSD license, all text here must be included in any redistribution.
+ *
+ */
+#ifndef WS_LOADING_TOOLTIPS_H
+#define WS_LOADING_TOOLTIPS_H
+
+#define WS_LOADING_TIP_1 "Name components in IO using emojis to differentiate them!"
+#define WS_LOADING_TIP_2 "WipperSnapper now supports TFT displays on some boards (more to come)."
+#define WS_LOADING_TIP_3 "Getting throttle errors? Try reducing a sensor's polling time."
+#define WS_LOADING_TIP_4 "\"Be quick, but don't hurry\" - John Wooden "
+
+#endif // WS_LOADING_TOOLTIPS_H
