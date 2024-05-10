17db34cca9c19f8a1e33ed216cfa495f8ad8c228
Tyeth Gundry <tyethgundry@googlemail.com>
2024-05-10 14:20:05 +0100

    refactor header file

--- src/nanopb/ws_pb_helpers.h
+++ src/nanopb/ws_pb_helpers.h
@@ -16,53 +16,11 @@
 #define WS_PB_ENCODE_H
 
 #include "pb.h"
-#include "pb_decode.h"
-#include "pb_encode.h"
-#include <Wippersnapper.h>
 
-
-// *****************************************************************************
-/*!
-    @brief    Decodes a protobuf message from a stream and prints any error.
-    @param    stream
-              The stream to decode from.
-    @param    fields
-              The protobuf message fields.
-    @param    dest_struct
-              The destination struct to decode into.
-    @return   True if decode was successful, false otherwise.
-!*/
-// *****************************************************************************
 bool ws_pb_decode(pb_istream_t *stream, const pb_msgdesc_t *fields,
-                         void *dest_struct) {
-  bool status = pb_decode(stream, fields, dest_struct);
-  if (!status) {
-    WS_DEBUG_PRINT("Protobuf decode error: ");
-    WS_DEBUG_PRINTLN(PB_GET_ERROR(stream));
-  }
-  return status;
-}
+                         void *dest_struct);
 
-// *****************************************************************************
-/*!
-    @brief    Encodes a protobuf message to a stream and prints any error.
-    @param    stream
-              The stream to encode to.
-    @param    fields
-              The protobuf message fields.
-    @param    src_struct
-              The source struct to encode from.
-    @return   True if encode was successful, false otherwise.
-!*/
-// *****************************************************************************
 bool ws_pb_encode(pb_ostream_t *stream, const pb_msgdesc_t *fields,
-                         const void *src_struct) {
-  bool status = pb_encode(stream, fields, src_struct);
-  if (!status) {
-    WS_DEBUG_PRINT("Protobuf encode error: ");
-    WS_DEBUG_PRINTLN(PB_GET_ERROR(stream));
-  }
-  return status;
-}
-
+                         const void *src_struct);
+                         
 #endif // WS_PB_ENCODE_H
