80b3cdfcac8f7fb8e542e54eb62decd6fd6c2813
tyeth <tyethgundry@googlemail.com>
2024-06-14 11:48:38 +0100

    Add WS_DELAY and RETRY_FUNCTION_UNTIL_TIMEOUT

--- src/Wippersnapper.h
+++ src/Wippersnapper.h
@@ -59,6 +59,56 @@
   {} ///< Prints line from debug output.
 #endif
 
+#define WS_DELAY_WITH_WDT(timeout)                                             \
+  {                                                                            \
+    unsigned long start = millis();                                            \
+    while (millis() - start < timeout) {                                       \
+      delay(10);                                                               \
+      yield();                                                                 \
+      feedWDT();                                                               \
+      if (millis() < start) {                                                  \
+        start = millis(); /* if rollover */                                    \
+      }                                                                        \
+    }                                                                          \
+  } ///< Delay function
+
+
+/**************************************************************************/
+/*!
+    @brief  Retry a function until a condition is met or a timeout is reached.
+    @param  func
+            The function to retry.
+    @param  result_type
+            The type of the result of the function.
+    @param  result_var
+            The variable to store the last result of the function.
+    @param  condition
+            The condition to check the result against.
+    @param  timeout
+            The maximum time to retry the function.
+    @param  interval
+            The time to wait between retries.
+    @param  ...
+            The arguments to pass to the function.
+*/
+/**************************************************************************/
+#define RETRY_FUNCTION_UNTIL_TIMEOUT(func, result_type, result_var, condition, \
+                                     timeout, interval, ...)                   \
+  {                                                                            \
+    unsigned long startTime = millis();                                        \
+    while (millis() - startTime < timeout) {                                   \
+      result_type result_var = func(__VA_ARGS__);                              \
+      if (condition(result_var)) {                                             \
+        break;                                                                 \
+      }                                                                        \
+      if (startTime < millis()) {                                              \
+        startTime = millis(); /* if rollover */                                \
+      }                                                                        \
+      WS_DELAY_WITH_WDT(interval);                                             \
+    }                                                                          \
+  } ///< Retry a function until a condition is met or a timeout is reached.
+
+
 // Wippersnapper pb helpers
 #include <nanopb/ws_pb_helpers.h>
 
