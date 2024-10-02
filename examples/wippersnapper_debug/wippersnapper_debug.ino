fe5f4d264dd6e64660937acd6359fa5d3758e544
Tyeth Gundry <tyethgundry@googlemail.com>
2024-10-02 17:22:34 +0100

    Update wippersnapper_debug.ino - ifdef esp32 coredump

--- examples/wippersnapper_debug/wippersnapper_debug.ino
+++ examples/wippersnapper_debug/wippersnapper_debug.ino
@@ -1,5 +1,8 @@
 // Adafruit IO WipperSnapper Beta (DEBUG BUILD ONLY!)
 // Brent Rubell for Adafruit Industries, 2021 - 2023
+#ifdef ARCH_ESP32
+#include "esp_core_dump.h"
+#endif
 
 #include "Wippersnapper_Networking.h"
 Wippersnapper_WiFi wipper;
@@ -8,10 +11,11 @@ Wippersnapper_WiFi wipper;
 #define WS_DEBUG
 
 void setup() {
-  #include "esp_core_dump.h"
-
+  
+#ifdef ARCH_ESP32
   // Configure the core dump to be saved to flash
   esp_err_t err = esp_core_dump_init(ESP_CORE_DUMP_FLASH); // Specify flash as storage
+#endif
 
   
   // Provisioning must occur prior to serial init.
@@ -21,10 +25,12 @@ void setup() {
   Serial1.begin(115200);  // ESP-IDF messages serial
   Serial1.setDebugOutput(true); // Enable ESP-IDF messages over Serial1
   //while (!Serial) delay(10);
-  
+
+#ifdef ARCH_ESP32
   if (err != ESP_OK) {
     Serial.println("Core dump init failed!");
   }
+#endif
   
   wipper.connect();
 }
