f4f7432cffe45a1b2774fbcb44ba597d52f5cf6a
brentru <brent@adafruit.com>
2024-10-08 12:37:23 -0400

    🚧 WIP, dsx - added message

--- src/components/ds18x20/controller.cpp
+++ src/components/ds18x20/controller.cpp
@@ -24,9 +24,18 @@ bool DS18X20Controller::Handle_Ds18x20Add(pb_istream_t *stream) {
     WS_DEBUG_PRINTLN("ERROR: Unable to decode Ds18x20Add message");
     return false;
   }
-  // Pass the decoded message to the hardware
-  // TODO: Brent you were here and looking at this:
-  // https://github.com/pstolarz/OneWireNg/blob/master/examples/arduino/DallasTemperature/DallasTemperature.ino#L125
+  // Extract the OneWire pin from the message
+  uint8_t pin_name = atoi(_DS18X20_model->GetDS18x20AddMsg()->onewire_pin + 1);
+
+  // Initialize the DS18X20Hardware object
+  DS18X20Hardware new_dsx_driver(pin_name);
+  new_dsx_driver.setResolution(
+      _DS18X20_model->GetDS18x20AddMsg()->sensor_resolution);
+
+  // Add the DS18X20Hardware object to the vector of hardware objects
+  _DS18X20_pins.push_back(new_dsx_driver);
+
+  // TODO: We should publish back an Added message to the broker
 
   return true;
 }
