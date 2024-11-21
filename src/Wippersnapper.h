ecc98848f2df1bca5d217158d3985df21cefb457
Tyeth Gundry <tyethgundry@googlemail.com>
2024-11-21 18:17:06 +0000

    Update Wippersnapper.h - bump version to 1.0.0-beta.94

--- src/Wippersnapper.h
+++ src/Wippersnapper.h
@@ -142,7 +142,7 @@
 #endif
 
 #define WS_VERSION                                                             \
-  "1.0.0-beta.93" ///< WipperSnapper app. version (semver-formatted)
+  "1.0.0-beta.94" ///< WipperSnapper app. version (semver-formatted)
 
 // Reserved Adafruit IO MQTT topics
 #define TOPIC_IO_THROTTLE "/throttle" ///< Adafruit IO Throttle MQTT Topic
