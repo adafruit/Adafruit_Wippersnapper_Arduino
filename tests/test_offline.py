45d23653ee591a1fe3f54e8ce0618ce0c53c568b
brentru <brent@adafruit.com>
2024-12-06 12:15:11 -0500

    [SD] Refactor checksum validation out, dont require it to operate

--- tests/test_offline.py
+++ tests/test_offline.py
@@ -20,11 +20,17 @@ def run_wokwi_cli(binary, timeout, scenario, diagram):
     )
     return result
 
-
 def test_invalid_json():
     result = run_wokwi_cli(f"tests/bin/offline/firmware.elf", "120000", f"tests/scenarios/offline/test-invalid-json.scenario.yaml", f"tests/diagrams/offline.json")
     assert result.returncode == 0
 
+def test_invalid_checksum():
+    result = run_wokwi_cli(f"tests/bin/offline/firmware.elf", "120000", f"tests/scenarios/offline/test-invalid-checksum.scenario.yaml", f"tests/diagrams/offline.json")
+    assert result.returncode == 0
+
+def test_valid_checksum():
+    result = run_wokwi_cli(f"tests/bin/offline/firmware.elf", "120000", f"tests/scenarios/offline/test-valid-checksum.scenario.yaml", f"tests/diagrams/offline.json")
+    assert result.returncode == 0
 
 def test_digital_input():
     result = run_wokwi_cli(f"tests/bin/offline/firmware.elf", "120000", f"tests/scenarios/offline/test-log-digital-in.scenario.yaml", f"tests/diagrams/offline.json")
@@ -34,7 +40,6 @@ def test_digital_input():
 def test_analog_input():
     result = run_wokwi_cli(f"tests/bin/offline/firmware.elf", "120000", f"tests/scenarios/offline/test-log-analogin.scenario.yaml", f"tests/diagrams/offline.json")
     assert result.returncode == 0
-
 
 def test_ds18b20():
     result = run_wokwi_cli(f"tests/bin/offline/firmware.elf", "120000", f"tests/scenarios/offline/test-log-ds18b20.scenario.yaml", f"tests/diagrams/offline.json")
