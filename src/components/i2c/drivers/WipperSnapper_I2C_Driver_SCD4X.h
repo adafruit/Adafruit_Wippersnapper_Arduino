9c9a98dd9bcee94e9c09a705a7577ea8e6eea484
Tyeth Gundry <tyethgundry@googlemail.com>
2025-01-31 14:40:41 +0000

    Update WipperSnapper_I2C_Driver_SCD4X.h - renamed include

--- src/components/i2c/drivers/WipperSnapper_I2C_Driver_SCD4X.h
+++ src/components/i2c/drivers/WipperSnapper_I2C_Driver_SCD4X.h
@@ -18,7 +18,7 @@
 #define WipperSnapper_I2C_Driver_SCD4X_H
 
 #include "WipperSnapper_I2C_Driver.h"
-#include <SensirionI2CScd4x.h>
+#include <SensirionI2cScd4x.h>
 #include <Wire.h>
 
 /**************************************************************************/
