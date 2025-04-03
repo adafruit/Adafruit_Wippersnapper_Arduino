e50a1648a9937d1df9aae02eb2d5cbd7e0ed07b7
tyeth <tyethgundry@googlemail.com>
2025-04-04 00:38:53 +0100

    Add INA260

--- src/components/i2c/WipperSnapper_I2C.h
+++ src/components/i2c/WipperSnapper_I2C.h
@@ -35,6 +35,7 @@
 #include "drivers/WipperSnapper_I2C_Driver_HTU21D.h"
 #include "drivers/WipperSnapper_I2C_Driver_HTU31D.h"
 #include "drivers/WipperSnapper_I2C_Driver_INA219.h"
+#include "drivers/WipperSnapper_I2C_Driver_INA260.h"
 #include "drivers/WipperSnapper_I2C_Driver_LC709203F.h"
 #include "drivers/WipperSnapper_I2C_Driver_LPS22HB.h"
 #include "drivers/WipperSnapper_I2C_Driver_LPS25HB.h"
@@ -150,6 +151,7 @@ private:
   WipperSnapper_I2C_Driver_HTU21D *_htu21d = nullptr;
   WipperSnapper_I2C_Driver_HTU31D *_htu31d = nullptr;
   WipperSnapper_I2C_Driver_INA219 *_ina219 = nullptr;
+  WipperSnapper_I2C_Driver_INA260 *_ina260 = nullptr;
   WipperSnapper_I2C_Driver_LTR329_LTR303 *_ltr329 = nullptr;
   WipperSnapper_I2C_Driver_LTR390 *_ltr390 = nullptr;
   WipperSnapper_I2C_Driver_MCP3421 *_mcp3421 = nullptr;
