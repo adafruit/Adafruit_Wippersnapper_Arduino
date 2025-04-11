0ece23be4088d5144788ff8c1df8e27d3ef5a36b
brentru <brent@adafruit.com>
2025-04-11 13:35:33 -0400

    test demo offline, nonet and netifaces

---
+++ examples/Wippersnapper_demo_offline_nonetiface/Wippersnapper_demo_offline_nonetiface.ino
@@ -0,0 +1,25 @@
+// Adafruit IO WipperSnapper
+// USE ONLY WITH DEVICES WITHOUT A NETWORK ADAPTER LIKE RP2040 PICO
+//
+// Adafruit invests time and resources providing this open source code.
+// Please support Adafruit and open source hardware by purchasing
+// products from Adafruit!
+//
+// Brent Rubell for Adafruit Industries, 2025
+//
+// All text above must be included in any redistribution.
+
+#include "ws_adapters.h"
+ws_adapter_offline wipper;
+#define WS_DEBUG // Enable debug output!
+#define BUILD_OFFLINE_ONLY
+
+void setup() {
+  Serial.begin(115200);
+  wipper.provision();
+  wipper.connect();
+}
+
+void loop() {
+  wipper.run();
+}
