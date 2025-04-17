58e273f1a21ec04eabc45829253ff569ad5a0552
brentru <brent@adafruit.com>
2025-04-17 14:32:43 -0400

    Fix - C6

--- src/provisioning/littlefs/WipperSnapper_LittleFS.cpp
+++ src/provisioning/littlefs/WipperSnapper_LittleFS.cpp
@@ -18,7 +18,8 @@
     defined(ARDUINO_ADAFRUIT_FEATHER_ESP32_V2) ||                              \
     defined(ARDUINO_ADAFRUIT_QTPY_ESP32_PICO) ||                               \
     defined(ARDUINO_ADAFRUIT_QTPY_ESP32C3) || defined(ARDUINO_ESP32_DEV) ||    \
-    defined(ESP32_DEV)
+    defined(ESP32_DEV) || \
+    defined(ARDUINO_ADAFRUIT_FEATHER_ESP32C6)
 #include "WipperSnapper_LittleFS.h"
 
 /**************************************************************************/
