c9a80703599b4e662c405ea72f7d95af32a41120
brentru <brent@adafruit.com>
2025-04-17 14:07:13 -0400

    Fix eval

--- src/provisioning/sdcard/ws_sdcard.cpp
+++ src/provisioning/sdcard/ws_sdcard.cpp
@@ -90,9 +90,9 @@ void ws_sdcard::calculateFileLimits() {
 bool ws_sdcard::InitDS1307() {
   _rtc_ds1307 = new RTC_DS1307();
   if (!_rtc_ds1307->begin()) {
-#if !defined(ARDUINO_ARCH_ESP8266) && !defined(ARDUINO_ARCH_SAMD) ||           \
-    !defined(ARDUINO_ADAFRUIT_FEATHER_ESP32C6) ||                              \
-        !defined(ARDUINO_ADAFRUIT_QTPY_ESP32C3)
+#if !defined(ARDUINO_ARCH_ESP8266) && !defined(ARDUINO_ARCH_SAMD) &&           \
+    !defined(ARDUINO_ADAFRUIT_FEATHER_ESP32C6) &&                              \
+    !defined(ARDUINO_ADAFRUIT_QTPY_ESP32C3)
     if (!_rtc_ds1307->begin(&Wire1)) {
       WS_DEBUG_PRINTLN("[SD] Runtime Error: Failed to initialize DS1307 RTC");
       delete _rtc_ds1307;
@@ -116,9 +116,9 @@ bool ws_sdcard::InitDS3231() {
   WS_DEBUG_PRINTLN("Begin DS3231 init");
   _rtc_ds3231 = new RTC_DS3231();
   if (!_rtc_ds3231->begin(&Wire)) {
-#if !defined(ARDUINO_ARCH_ESP8266) && !defined(ARDUINO_ARCH_SAMD) ||           \
-    !defined(ARDUINO_ADAFRUIT_FEATHER_ESP32C6) ||                              \
-        !defined(ARDUINO_ADAFRUIT_QTPY_ESP32C3)
+#if !defined(ARDUINO_ARCH_ESP8266) && !defined(ARDUINO_ARCH_SAMD) &&           \
+    !defined(ARDUINO_ADAFRUIT_FEATHER_ESP32C6) &&                              \
+    !defined(ARDUINO_ADAFRUIT_QTPY_ESP32C3)
     if (!_rtc_ds3231->begin(&Wire1)) {
       WS_DEBUG_PRINTLN("[SD] Runtime Error: Failed to initialize DS3231 RTC");
       delete _rtc_ds3231;
@@ -143,9 +143,9 @@ bool ws_sdcard::InitPCF8523() {
   if (!_rtc_pcf8523->begin(&Wire)) {
     WS_DEBUG_PRINTLN(
         "[SD] Runtime Error: Failed to initialize PCF8523 RTC on WIRE");
-#if !defined(ARDUINO_ARCH_ESP8266) && !defined(ARDUINO_ARCH_SAMD) ||           \
-    !defined(ARDUINO_ADAFRUIT_FEATHER_ESP32C6) ||                              \
-        !defined(ARDUINO_ADAFRUIT_QTPY_ESP32C3)
+#if !defined(ARDUINO_ARCH_ESP8266) && !defined(ARDUINO_ARCH_SAMD) &&           \
+    !defined(ARDUINO_ADAFRUIT_FEATHER_ESP32C6) &&                              \
+    !defined(ARDUINO_ADAFRUIT_QTPY_ESP32C3)
     if (!_rtc_pcf8523->begin(&Wire1)) {
       WS_DEBUG_PRINTLN(
           "[SD] Runtime Error: Failed to initialize PCF8523 RTC on WIRE1");
