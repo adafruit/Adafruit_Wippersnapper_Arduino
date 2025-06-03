d8727d7075b7fa882f71461d8e855b5f326088f5
tyeth <tyethgundry@googlemail.com>
2025-06-03 21:00:26 +0100

    Add Sparkle Motion Stick

--- src/provisioning/littlefs/WipperSnapper_LittleFS.cpp
+++ src/provisioning/littlefs/WipperSnapper_LittleFS.cpp
@@ -18,6 +18,7 @@
     defined(ARDUINO_ADAFRUIT_FEATHER_ESP32_V2) ||                              \
     defined(ARDUINO_ADAFRUIT_QTPY_ESP32_PICO) ||                               \
     defined(ARDUINO_SPARKLEMOTIONMINI_ESP32) ||                                \
+    defined(ARDUINO_SPARKLEMOTIONSTICK_ESP32) ||                               \
     defined(ARDUINO_ADAFRUIT_QTPY_ESP32C3) ||                                  \
     defined(ARDUINO_ADAFRUIT_FEATHER_ESP32C6)
 #include "WipperSnapper_LittleFS.h"
