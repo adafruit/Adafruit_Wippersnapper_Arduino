0bec5c270d00bee9c812b9266ff136627ba46806
tyeth <tyethgundry@googlemail.com>
2025-06-20 18:43:29 +0100

    doxygen

--- src/components/i2c/drivers/WipperSnapper_I2C_Driver_Out_Sh1107.h
+++ src/components/i2c/drivers/WipperSnapper_I2C_Driver_Out_Sh1107.h
@@ -189,8 +189,8 @@ public:
 
   /*!
       @brief    Writes a message to the fake "SSD1306" SH1107 display.
-      @param    msg_write
-                  Pointer to a wippersnapper_i2c_v1_SSD1306Write message.
+      @param    message
+                  The message to be displayed.
   */
   void WriteMessageSSD1306(const char *message) {
     // This is a SH1107, not a SSD1306, so we just call the SH1107 write
