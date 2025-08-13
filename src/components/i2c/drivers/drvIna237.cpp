0c34fe51705f974b6fbe2b2b618cba472627084f
tyeth <tyethgundry@googlemail.com>
2025-08-13 13:34:18 +0100

    Refactor INA260 and add INA237/238

---
+++ src/components/i2c/drivers/drvIna237.cpp
@@ -0,0 +1,82 @@
+/*!
+ * @file drvIna237.cpp
+ *
+ * Device driver for the INA237 High Precision DC Current and Voltage Monitor
+ *
+ * Adafruit invests time and resources providing this open source code,
+ * please support Adafruit and open-source hardware by purchasing
+ * products from Adafruit!
+ *
+ * Copyright (c) Tyeth Gundry 2025 for Adafruit Industries.
+ *
+ * MIT license, all text here must be included in any redistribution.
+ *
+ */
+
+#include "drvIna237.h"
+#include <Adafruit_INA237.h>
+
+/*******************************************************************************/
+/*!
+    @brief    Destructor for an INA237 sensor.
+*/
+/*******************************************************************************/
+drvIna237::~drvIna237() {
+  if (_ina237) {
+    delete _ina237;
+    _ina237 = nullptr;
+  }
+}
+
+/*******************************************************************************/
+/*!
+    @brief    Initializes the INA237 sensor and begins I2C.
+    @returns  True if initialized successfully, False otherwise.
+*/
+/*******************************************************************************/
+bool drvIna237::begin() {
+  _ina237 = new Adafruit_INA237();
+  if (!_ina237->begin(_address, _i2c)) {
+    WS_DEBUG_PRINTLN("INA237 failed to initialise!");
+    return false;
+  }
+  // TODO: use setCalibration()
+
+  return true;
+}
+
+/*******************************************************************************/
+/*!
+    @brief    Reads a voltage sensor and converts the
+              reading into the expected SI unit.
+    @param    voltageEvent
+              voltage sensor reading, in volts.
+    @returns  True if the sensor event was obtained successfully, False
+              otherwise.
+*/
+/*******************************************************************************/
+bool drvIna237::getEventVoltage(sensors_event_t *voltageEvent) {
+  voltageEvent->voltage = _ina237->getBusVoltage_V();
+  return true;
+}
+
+/**
+ * @brief   Get the current sensor event.
+ *
+ * @param   currentEvent  Pointer to the current sensor event.
+ *
+ * @returns True if the sensor event was obtained successfully, False
+ * otherwise.
+ */
+bool drvIna237::getEventCurrent(sensors_event_t *currentEvent) {
+  currentEvent->current = _ina237->getCurrent_mA();
+  return true;
+}
+
+void drvIna237::ConfigureDefaultSensorTypes() {
+  _default_sensor_types_count = 2;
+  _default_sensor_types[0] =
+      wippersnapper_sensor_SensorType_SENSOR_TYPE_VOLTAGE;
+  _default_sensor_types[1] =
+      wippersnapper_sensor_SensorType_SENSOR_TYPE_CURRENT;
+}
---
+++ src/components/i2c/drivers/drvIna260.cpp
@@ -0,0 +1,82 @@
+/*!
+ * @file drvIna260.cpp
+ *
+ * Device driver for the INA260 DC Current and Voltage Monitor
+ *
+ * Adafruit invests time and resources providing this open source code,
+ * please support Adafruit and open-source hardware by purchasing
+ * products from Adafruit!
+ *
+ * Copyright (c) Tyeth Gundry 2025 for Adafruit Industries.
+ *
+ * MIT license, all text here must be included in any redistribution.
+ *
+ */
+
+#include "drvIna260.h"
+#include <Adafruit_INA260.h>
+
+/*******************************************************************************/
+/*!
+    @brief    Destructor for an INA260 sensor.
+*/
+/*******************************************************************************/
+drvIna260::~drvIna260() {
+  if (_ina260) {
+    delete _ina260;
+    _ina260 = nullptr;
+  }
+}
+
+/*******************************************************************************/
+/*!
+    @brief    Initializes the INA260 sensor and begins I2C.
+    @returns  True if initialized successfully, False otherwise.
+*/
+/*******************************************************************************/
+bool drvIna260::begin() {
+  _ina260 = new Adafruit_INA260();
+  if (!_ina260->begin(_address, _i2c)) {
+    WS_DEBUG_PRINTLN("INA260 failed to initialise!");
+    return false;
+  }
+  // TODO: use setCalibration()
+
+  return true;
+}
+
+/*******************************************************************************/
+/*!
+    @brief    Reads a voltage sensor and converts the
+              reading into the expected SI unit.
+    @param    voltageEvent
+              voltage sensor reading, in volts.
+    @returns  True if the sensor event was obtained successfully, False
+              otherwise.
+*/
+/*******************************************************************************/
+bool drvIna260::getEventVoltage(sensors_event_t *voltageEvent) {
+  voltageEvent->voltage = _ina260->readBusVoltage();
+  return true;
+}
+
+/**
+ * @brief   Get the current sensor event.
+ *
+ * @param   currentEvent  Pointer to the current sensor event.
+ *
+ * @returns True if the sensor event was obtained successfully, False
+ * otherwise.
+ */
+bool drvIna260::getEventCurrent(sensors_event_t *currentEvent) {
+  currentEvent->current = _ina260->readCurrent();
+  return true;
+}
+
+void drvIna260::ConfigureDefaultSensorTypes() {
+  _default_sensor_types_count = 2;
+  _default_sensor_types[0] =
+      wippersnapper_sensor_SensorType_SENSOR_TYPE_VOLTAGE;
+  _default_sensor_types[1] =
+      wippersnapper_sensor_SensorType_SENSOR_TYPE_CURRENT;
+}
