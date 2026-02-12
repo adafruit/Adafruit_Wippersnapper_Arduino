cb0cb9c9f55cee89060c78ec5693f80fbc924f15
brentru <brent@adafruit.com>
2026-02-12 13:55:51 -0500

    Correc thte demo code

--- examples/Wippersnapper_demo/Wippersnapper_demo.ino
+++ examples/Wippersnapper_demo/Wippersnapper_demo.ino
@@ -1,39 +1,22 @@
 // Adafruit IO WipperSnapper
 //
-// This sketch is for devices which lack USB-MSD or LittleFS support such
-// as the ESP32Dev for Wokwi Simulator
-//
 // Adafruit invests time and resources providing this open source code.
 // Please support Adafruit and open source hardware by purchasing
 // products from Adafruit!
 //
-// Brent Rubell for Adafruit Industries, 2025
+// Brent Rubell for Adafruit Industries, 2026
 //
 // All text above must be included in any redistribution.
 
 #include "ws_platforms.h"
-#define WS_DEBUG // Enable debug output
-/************************ Adafruit IO Config *******************************/
-
-// Visit io.adafruit.com if you need to create an account,
-// or if you need your Adafruit IO key.
-#define IO_USERNAME "YOUR_AIO_USERNAME"
-#define IO_KEY "YOUR_AIO_KEY"
-#define IO_URL "io.adafruit.com"
-#define IO_PORT 8883
-/**************************** WiFi Config ***********************************/
-#define WIFI_SSID "YOUR_WIFI_SSID"
-#define WIFI_PASS "YOUR_WIFI_PASSWORD"
-
-ws_adapter_wifi wipper(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS, IO_URL, IO_PORT);
+ws_adapter_wifi wipper;
+// Uncomment the following line to use the offline adapter for Pico
+// ws_adapter_offline wipper;
+#define WS_DEBUG // Enable debug output!
 
 void setup() {
-  // Provisioning must occur prior to serial init.
-  wipper.provision();
-
   Serial.begin(115200);
-  // while (!Serial) delay(10);
-
+  wipper.provision();
   wipper.connect();
 }
 
