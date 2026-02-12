5b7e8bd2cb0c07867160ada0a4d7d636f8323fd9
brentru <brent@adafruit.com>
2026-02-12 13:50:47 -0500

    reorg demo build

--- src/Wippersnapper_demo.ino
+++ src/Wippersnapper_demo.ino
@@ -9,9 +9,9 @@
 // All text above must be included in any redistribution.
 
 #include "ws_platforms.h"
-//ws_adapter_wifi wipper;
+ws_adapter_wifi wipper;
 // Uncomment the following line to use the offline adapter for Pico
-ws_adapter_offline wipper;
+// ws_adapter_offline wipper;
 #define WS_DEBUG // Enable debug output!
 
 void setup() {
