527d12d5c8b804113658dca53707994dfc77de6e
brentru <brent@adafruit.com>
2026-02-19 17:03:31 -0500

    clang

--- src/wippersnapper.h
+++ src/wippersnapper.h
@@ -30,9 +30,7 @@
 */
 #ifdef WS_DEBUG
 #define WS_DEBUG_PRINT(...)                                                    \
-  {                                                                            \
-    WS_PRINTER.print(__VA_ARGS__);                                             \
-  } /**< Print debug message to serial */
+  { WS_PRINTER.print(__VA_ARGS__); } /**< Print debug message to serial */
 #define WS_DEBUG_PRINTLN(...)                                                  \
   {                                                                            \
     WS_PRINTER.println(__VA_ARGS__);                                           \
@@ -44,11 +42,9 @@
   } /**< Print debug message in hexadecimal */
 #else
 #define WS_DEBUG_PRINT(...)                                                    \
-  {                                                                            \
-  } /**< Debug print */
+  {} /**< Debug print */
 #define WS_DEBUG_PRINTLN(...)                                                  \
-  {                                                                            \
-  } /**< Debug println */
+  {} /**< Debug println */
 #endif
 
 /*!
